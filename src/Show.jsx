aadc2d25d54a6f8b5ed034aeb42b5f479eb0d8e5
cjdemille <cjdemille5@gmail.com>
2023-11-29 11:57:24 -0500

    add searching fucntion

--- src/Show.jsx
+++ src/Show.jsx
@@ -10,14 +10,13 @@ import './Show.css';
 
 
 export default function Show({name='TestName', rating=5, img = "https://static.tvmaze.com/uploads/images/medium_portrait/401/1003373.jpg" , summary="lorem"}){
-    const style = {width: '18rem'}
     return(
         <div className="Show">
-            <div className="card" style ={style}>
+            <div className="card" >
                 <img src={img} alt="" className="card-img-top" />
                 <div className="card-body">
                     <h2 className="card-header">{name}</h2>
-                    <p>Average Rating: {rating}</p>
+                   {rating && <p>Average Rating: {rating}</p> } 
                 </div>
             </div>
         </div>
